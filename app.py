c9161baf5d34be9f1cd44cf1a15a6e886bb5aced
shemanto27 <shemantosharkarofficial@gmail.com>
2024-07-15 23:11:12 +0600

    session

--- app.py
+++ app.py
@@ -1,21 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, request, url_for
 
 app = Flask(__name__)
 
+app.secret_key = "2e9ac41b1e0b66a8d93d66400e2300c4b4c2953f"
 PLACEHOLDER_CODE =  "print('Hello, World!')"
 
 @app.context_processor
 def context():
+    if session.get("code") is None:
+        session["code"] = PLACEHOLDER_CODE
     return {
         "message" : "Paste Your Python Code Here 🐍",
-        "code" : PLACEHOLDER_CODE,
+        "code" : session["code"],
     }
 
 
 @app.route('/', methods=["GET"])
 def code():
     return render_template("code_input.html") 
-# The ** unpacks the dictionary context so that each key-value pair in the dictionary is passed as a separate keyword argument to the render_template function.
+
+
+@app.route("/save_code", methods=["POST"])
+def save_code():
+    session["code"] = request.form.get("code")
+    return redirect(url_for("code"))
+
+@app.route("/reset_session", methods=["POST"])
+def reset_session():
+    session.clear()
+    session["code"] = PLACEHOLDER_CODE
+    return redirect(url_for("code"))
 
 
 
